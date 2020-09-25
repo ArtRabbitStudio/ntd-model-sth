b9f4ffb3f0318b6851e7abedb4cb5fa4b89dd026
Igor Clark <igor@artrabbit.com>
2020-09-25 18:21:45 +0100

    rm logging

--- sth_run.py
+++ sth_run.py
@@ -1,8 +1,6 @@
 from sth_simulation.helsim_RUN import STH_Simulation
 
-print('pre-run')
 df = STH_Simulation(paramFileName='AscarisParameters_high.txt', demogName='KenyaKDHS', numReps=100)
-print('post-run')
 
 df.to_json('sth_results.json')
 
